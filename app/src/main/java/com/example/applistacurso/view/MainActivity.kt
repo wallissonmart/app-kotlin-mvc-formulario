00b47f7aa37b47311a6357a3be73cfa05ea1bc74
Wallisson Martins <wallissonmartins37@gmail.com>
2023-10-23 17:42:32 -0300

    config

--- app/src/main/java/com/example/applistacurso/view/MainActivity.kt
+++ app/src/main/java/com/example/applistacurso/view/MainActivity.kt
@@ -1,39 +1,78 @@
 package com.example.applistacurso.view
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.applistacurso.databinding.ActivityMainBinding
 import com.example.applistacurso.model.Curso
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var appPreferences: SharedPreferences
+    private var isDarkThemeEnabled = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        appPreferences = getSharedPreferences("AppListaCursoPreferences", MODE_PRIVATE)
+        isDarkThemeEnabled = appPreferences.getBoolean("isDarkTheme", false)
+
+        getThemeEnabled()
         setupListeners()
+        toggleDarkThemeSwitch()
     }
 
     private fun setupListeners() {
         val buttonFinalizar = binding.buttonFinalizar
         buttonFinalizar.setOnClickListener {
-            handleButtonClick()
+            handleFinalizarClick()
         }
     }
 
-    private fun handleButtonClick() {
-        val nome = binding.editTextPrimeiroNome.text.toString()
-        val sobrenome = binding.editTextSobrenome.text.toString()
-        val cursoInteressado = binding.editTextNomeCurso.text.toString()
-        val telefone = binding.editTextTelefone.text.toString()
+    private fun handleFinalizarClick() {
+        val firstName = binding.editTextPrimeiroNome.text.toString()
+        val lastName = binding.editTextSobrenome.text.toString()
+        val desiredCourse = binding.editTextNomeCurso.text.toString()
+        val phone = binding.editTextTelefone.text.toString()
 
-        val curso = Curso(nome, sobrenome, cursoInteressado, telefone)
+        val curso = Curso(firstName, lastName, desiredCourse, phone)
 
         Toast.makeText(this, curso.primeiroNome, Toast.LENGTH_LONG).show()
         printCursoDetails(curso)
+    }
+
+    private fun getThemeEnabled() {
+        val switchMode = binding.switchMode
+        switchMode.isActivated = isDarkThemeEnabled
+
+        if (isDarkThemeEnabled) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+    }
+
+    private fun toggleDarkThemeSwitch() {
+        val switchMode = binding.switchMode
+
+        switchMode.setOnCheckedChangeListener { _, isChecked ->
+            isDarkThemeEnabled = isChecked
+
+            val editor = appPreferences.edit()
+            editor.putBoolean("isDarkTheme", isDarkThemeEnabled)
+            editor.apply()
+
+            if (isDarkThemeEnabled) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
     }
 
     private fun printCursoDetails(curso: Curso) {
